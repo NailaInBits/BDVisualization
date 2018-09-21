f1ffd33c3470f19ec307a9cfe8e8df597f415b5a
nishata <nishatnanjum@gmail.com>
2018-09-20 21:34:21 -0400

    Added a chord diagram :bowtie:

--- resources/scripts/mapVisualization.js
+++ resources/scripts/mapVisualization.js
@@ -13,7 +13,7 @@ var mapWidth = 1350,
 // Map sizing, numbers based on stylistic placement.
 var projection = d3.geoMercator()
     .scale(150)
-    .translate([mapWidth / 5, mapHeight / 3]);
+    .translate([mapWidth / 3, mapHeight / 2]);
 
 var path = d3.geoPath()
     .pointRadius(2)
@@ -27,41 +27,7 @@ var svg = d3.select(".map")
     .attr('transform', 'translate(' + margin.left + "," + margin.top + ')');
 
 var worldMapJson = "resources/styles/WorldMap.json";
-
 var populationsJson = "resources/styles/Populations.json";
-
-var timeAxisHeight = 20;
-
-/***** Sunburst Variable Definitions *****/
-var sunburstWidth = 760,
-    sunburstHeight = 500,
-    sunburstRadius = (Math.min(sunburstWidth, sunburstHeight) / 2) - 10;
-
-var formatNumber = d3.format(",d");
-
-var x = d3.scaleLinear()
-    .range([0, 2 * Math.PI]);
-
-var y = d3.scaleSqrt()
-    .range([0, sunburstRadius]);
-
-var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-var partition = d3.partition();
-
-var arc = d3.arc()
-    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
-    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
-
-var svgSunburst = d3.select(".sunburst")
-    .append("svg")
-    .attr("sunburstWidth", sunburstWidth)
-    .attr("sunburstHeight", sunburstHeight)
-    .append("g")
-    .attr("transform", "translate(" + sunburstWidth / 2 + "," + (sunburstHeight / 2) + ")");
-
 var jobsJson = "resources/styles/OccupationBreakdown.json";
 
 /***** Miscellaneous Variable Definitions *****/
@@ -73,8 +39,6 @@ var tooltip = d3.select(".map")
 // Colors for the legend
 var legendColor = d3.scaleLinear()
     .range(['rgb(0,0,255)','rgb(255,255,0)']);
-
-var legendText = ["Client Location", "Pop Location"];
 
 d3.json(worldMapJson, function(error, countries) {
 	d3.json(populationsJson, function(error, populations) {
@@ -105,7 +69,8 @@ d3.json(worldMapJson, function(error, countries) {
 			});
 		svg.call(zoom);
 
-		d3.csv("resources/styles/ImmigrationRoutes.csv",row => {
+		d3.csv("resources/style/BDLatLon.csv", function(error,bd) {
+		    d3.csv("resources/styles/ImmigrationRoutes.csv",row => {
 				row.Year = +row.Year;
 				row['num_of_people'] = +row['num_of_people']; return row;
 		}, function(data) {
@@ -169,49 +134,196 @@ d3.json(worldMapJson, function(error, countries) {
 						.attr("cy", projection([data[i].longitude, data[i].latitude])[1])
 		    			.attr("r", (Math.sqrt(data[i]['num_of_people']))/15)
 						.style("fill", "red");
-                    map.append("line")
-                        .attr("x1", d=>projection([data[i].longitude, data[i].latitude])[0])
-                        .attr("y1", d=>projection([data[i].longitude, data[i].latitude])[1])
-                        .attr("x2", d=>projection([data[i].longitude, data[i].latitude])[0])
-                        .attr("y2", d=>projection([data[i].longitude, data[i].latitude])[1])
-                        .style("fill", "red")
-                        .attr("stroke-width", 10.5);
 				}
 
 				let causeArr = causeArr2.find(d => +d.time === year);
 			}
 			render(2017);
 		});
+		});
 
-        // Sunburst Diagram
-        d3.json(jobsJson, function(error, root) {
-            if (error) throw error;
-
-            root = d3.hierarchy(root);
-            root.sum(function(d) { return d.size; });
-            svgSunburst.selectAll("path")
-                .data(partition(root).descendants())
-                .enter().append("path")
-                .attr("d", arc)
-                .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
-                .on("click", click)
-                .append("title")
-                .text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
+        //Male Chord Diagram
+        d3.csv('resources/styles/male.csv', function(error, data) {
+            mpr = chordMpr(data);
+            mpr.addValuesToMap('root')
+            .addValuesToMap('node')
+            .setFilter(function(row, a, b) {
+                return (row.root === a.name && row.node === b.name)
+            })
+            .setAccessor(function(recs, a, b) {
+                if (!recs[0]) return 0;
+                return +recs[0].count;
+            });
+            drawChords(mpr.getMatrix(), mpr.getMap());
         });
 
-        function click(d) {
-            svgSunburst.transition()
-                .duration(750)
-                .tween("scale", function() {
-                    var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-                        yd = d3.interpolate(y.domain(), [d.y0, 1]),
-                        yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, sunburstRadius]);
-                    return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+        function drawChords(matrix, mmap) {
+            var w = 680,
+                h = 600,
+                r1 = h / 3,
+                r0 = r1 - 10;
+
+            var chord = d3.chord()
+                .padAngle(0.05)
+                .sortSubgroups(d3.descending)
+                .sortChords(d3.descending);
+
+            var arc = d3.arc()
+                .innerRadius(r0)
+                .outerRadius(r0 + 20);
+
+            var ribbon = d3.ribbon()
+                .radius(r0);
+
+            var svg = d3.select(".chordM").append("svg:svg")
+                        .attr("width", w)
+                        .attr("height", h)
+                        .append("svg:g")
+                        .attr("id", "circle")
+                        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+                        .datum(chord(matrix));
+
+            svg.append("circle")
+               .attr("r", r0 + 20);
+
+            var mapReader = chordRdr(matrix, mmap);
+            var g = svg.selectAll("g.group")
+                .data(function(chords) {
+                    return chords.groups;
                 })
-            .selectAll("path")
-            .attrTween("d", function(d) { return function() { return arc(d); }; });
+                .enter().append("svg:g")
+                .attr("class", "group")
+
+            g.append("svg:path")
+                .style("stroke", "grey")
+                .style("fill", function(d) {
+                            return mapReader(d).gdata;
+                        })
+                        .attr("d", arc);
+
+            g.append("svg:text")
+            .each(function(d) {
+                d.angle = (d.startAngle + d.endAngle) / 2;
+            })
+            .attr("dy", ".35em")
+            .style("font-family", "helvetica, arial, sans-serif")
+            .style("font-size", "9px")
+            .attr("text-anchor", function(d) {
+                            return d.angle > Math.PI ? "end" : null;
+            })
+            .attr("transform", function(d) {
+                            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")" +
+                                "translate(" + (r0 + 26) + ")" +
+                                (d.angle > Math.PI ? "rotate(180)" : "");
+                        })
+            .text(function(d) {
+                            return mapReader(d).gname;
+                        });
+
+            var colors = d3.scaleOrdinal(d3.schemeCategory20c);
+            var chordPaths = svg.selectAll("path.chord")
+                .data(function(chords) {
+                    return chords;
+                })
+                .enter().append("svg:path")
+                .attr("class", "chord")
+                .style("stroke", "grey")
+                .style("fill", function(d, i) {
+                    return colors(i)
+                })
+                .attr("d", ribbon.radius(r0))
         }
 
-        d3.select(self.frameElement).style("sunburstHeight", sunburstHeight + "px");
+        //Female Chord Diagram
+        d3.csv('resources/styles/female.csv', function(error, data) {
+            fmpr = chordMpr(data);
+            fmpr.addValuesToMap('root')
+            .addValuesToMap('node')
+            .setFilter(function(row, a, b) {
+                return (row.root === a.name && row.node === b.name)
+            })
+            .setAccessor(function(recs, a, b) {
+                if (!recs[0]) return 0;
+                return +recs[0].count;
+            });
+            drawFemaleChords(fmpr.getMatrix(), fmpr.getMap());
+        });
+
+        function drawFemaleChords(matrix, mmap) {
+            var w = 680,
+                h = 600,
+                r1 = h / 3,
+                r0 = r1 - 10;
+
+            var chord = d3.chord()
+                .padAngle(0.05)
+                .sortSubgroups(d3.descending)
+                .sortChords(d3.descending);
+
+            var arc = d3.arc()
+                .innerRadius(r0)
+                .outerRadius(r0 + 20);
+
+            var ribbon = d3.ribbon()
+                .radius(r0);
+
+            var svg = d3.select(".chordF").append("svg:svg")
+                        .attr("width", w)
+                        .attr("height", h)
+                        .append("svg:g")
+                        .attr("id", "circle")
+                        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")")
+                        .datum(chord(matrix));
+
+            svg.append("circle")
+               .attr("r", r0 + 20);
+
+            var mapReader = chordRdr(matrix, mmap);
+            var g = svg.selectAll("g.group")
+                .data(function(chords) {
+                    return chords.groups;
+                })
+                .enter().append("svg:g")
+                .attr("class", "group")
+
+            g.append("svg:path")
+                .style("stroke", "grey")
+                .style("fill", function(d) {
+                            return mapReader(d).gdata;
+                        })
+                        .attr("d", arc);
+
+            g.append("svg:text")
+            .each(function(d) {
+                d.angle = (d.startAngle + d.endAngle) / 2;
+            })
+            .attr("dy", ".35em")
+            .style("font-family", "helvetica, arial, sans-serif")
+            .style("font-size", "9px")
+            .attr("text-anchor", function(d) {
+                            return d.angle > Math.PI ? "end" : null;
+            })
+            .attr("transform", function(d) {
+                            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")" +
+                                "translate(" + (r0 + 26) + ")" +
+                                (d.angle > Math.PI ? "rotate(180)" : "");
+                        })
+            .text(function(d) {
+                            return mapReader(d).gname;
+                        });
+
+            var colors = d3.scaleOrdinal(d3.schemeCategory20c);
+            var chordPaths = svg.selectAll("path.chord")
+                .data(function(chords) {
+                    return chords;
+                })
+                .enter().append("svg:path")
+                .attr("class", "chord")
+                .style("stroke", "grey")
+                .style("fill", function(d, i) {
+                    return colors(i)
+                })
+                .attr("d", ribbon.radius(r0))
+        }
 	});
 });
